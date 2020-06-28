82f60447705abb684248237c50b5fe9c87d28f27
Bogdan Grigorescu <bogdan.grigorescu93@gmail.com>
2020-06-28 12:43:14 +0300

    Add update-config

---
+++ pkg/update-configs.sh
@@ -0,0 +1,15 @@
+mkdir -p \
+  ../dotfiles \
+  ../dotfiles/lf \
+  ../dotfiles/zsh \
+  ../dotfiles/nvim \
+  ../dotfiles/alacritty
+
+cp ${HOME}/.zshrc ./dotfiles/
+cp ${HOME}/.gitconfig ./dotfiles/
+cp ${HOME}/.xinitrc ./dotfiles/
+
+cp -rv ${HOME}/.config/lf ../dotfiles/lf
+cp -rv ${HOME}/.config/zsh ../dotfiles/zsh
+cp -rv ${HOME}/.config/nvim ../dotfiles/nvim
+cp -rv ${HOME}/.config/alacritty ../dotfiles/alacritty
